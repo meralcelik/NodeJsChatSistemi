c734b2255488d99c043290259c90463703a08403
meralcelik <meralcelik1444@gmail.com>
2021-09-15 18:30:37 +0300

    chat ekranı yapıldı

--- app.js
+++ app.js
@@ -1,34 +1,69 @@
-const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
+
 const logger = require('morgan');
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const dotenv= require('dotenv');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const passport = require('passport');
+
+
+const dotenv = require('dotenv');
 dotenv.config();
+
+const index = require('./routes/index');
+const auth = require('./routes/auth');
+const chat = require('./routes/chat');
+
+
 const app = express();
+
+// helpers
+const db = require('./helpers/db')();
+
+// middlewares
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'bower_components')));
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+// express-session
+app.use(session({
+
+	secret: process.env.SESSION_SECRET_KEY,
+	resave: false,
+	saveUninitialized: true,
+	cookie: { maxAge: 14 * 24 * 3600000  }
+}));
+
+// passport.js
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/', index);
+app.use('/auth', auth);
+app.use('/chat', chat);
+
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
